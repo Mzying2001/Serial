ad6afc1fb039f540011fec22da4ef0e7604718d5
enterausername11111 <u4i_mkb0nwvw5@dingtalk.com>
2023-06-05 10:14:43 +0800

    修复只能识别到一个串口的bug

--- Serial/SerialPortInfo.cs
+++ Serial/SerialPortInfo.cs
@@ -21,22 +21,39 @@ namespace Serial
         public static List<SerialPortInfo> GetSerialPortInfoList()
         {
             List<SerialPortInfo> list = new List<SerialPortInfo>();
-            try
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%(COM%)'"))
             {
-                using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_SerialPort"))
+                foreach (ManagementObject obj in searcher.Get())
                 {
-                    foreach (ManagementObject obj in searcher.Get())
+                    string deviceName = obj["Name"].ToString();
+                    string portName = ExtractPortName(deviceName);
+                    string description = obj["Description"].ToString();
+
+                    list.Add(new SerialPortInfo()
                     {
-                        list.Add(new SerialPortInfo()
-                        {
-                            PortName = obj["DeviceID"].ToString(),
-                            Description = obj["Description"].ToString()
-                        });
-                    }
+                        PortName = portName,
+                        Description = description
+                    });
                 }
             }
-            catch { }
             return list;
+        }
+
+        private static string ExtractPortName(string deviceName)
+        {
+            // Assuming the format is "<SomeText> (<PortName>)"
+            int startIndex = deviceName.LastIndexOf('(');
+            int endIndex = deviceName.LastIndexOf(')');
+
+            if (startIndex != -1 && endIndex != -1 && startIndex < endIndex)
+            {
+                int portNameLength = endIndex - startIndex - 1;
+                string portName = deviceName.Substring(startIndex + 1, portNameLength);
+                return portName;
+            }
+
+            // If the expected format is not found, return the entire device name
+            return deviceName;
         }
     }
 }
