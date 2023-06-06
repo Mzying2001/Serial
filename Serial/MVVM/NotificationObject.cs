183d0d371a88b025d05ea010e444a3b5fd9d4223
Mzying2001 <Mzying2001@qq.com>
2023-06-06 22:31:52 +0800

    添加ExecuteOnUIThread

--- Serial/MVVM/NotificationObject.cs
+++ Serial/MVVM/NotificationObject.cs
@@ -1,11 +1,36 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace Serial.MVVM
 {
     public class NotificationObject : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private Dispatcher UIThreadDispatcher { get; set; }
+
+        public NotificationObject()
+        {
+            SetUIThreadDispatcher(Dispatcher.CurrentDispatcher);
+        }
+
+        public void SetUIThreadDispatcher(Dispatcher dispatcher)
+        {
+            UIThreadDispatcher = dispatcher;
+        }
+
+        protected void ExecuteOnUIThread(Action action)
+        {
+            UIThreadDispatcher.Invoke(action);
+        }
+
+        protected async Task ExecuteOnUIThreadAsync(Action action)
+        {
+            await UIThreadDispatcher.InvokeAsync(action);
+        }
 
         protected void RaisePropertyChanged([CallerMemberName] string propName = null)
         {
