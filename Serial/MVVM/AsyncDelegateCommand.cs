c558c4ccd771573b99f2e16de3b8cadbabb18ca1
Mzying2001 <Mzying2001@qq.com>
2023-06-07 23:17:00 +0800

    添加AsyncDelegateCommand

---
+++ Serial/MVVM/AsyncDelegateCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Serial.MVVM
+{
+    public class AsyncDelegateCommand : DelegateCommand
+    {
+        public new Func<object, Task> Execute { get; set; }
+
+        private async void CallAsyncExecute(object parameter)
+        {
+            await Execute(parameter);
+        }
+
+        public AsyncDelegateCommand(Func<object, Task> execute) : this(execute, true)
+        {
+        }
+
+        public AsyncDelegateCommand(Func<object, Task> execute, bool canExecute) : base((Action<object>)null, canExecute)
+        {
+            Execute = execute;
+            base.Execute = CallAsyncExecute;
+        }
+
+        public AsyncDelegateCommand(Func<Task> execute) : this(execute, true)
+        {
+        }
+
+        public AsyncDelegateCommand(Func<Task> execute, bool canExecute) : base((Action<object>)null, canExecute)
+        {
+            Execute = async _ => await execute();
+            base.Execute = CallAsyncExecute;
+        }
+    }
+}
---
+++ Serial/MVVM/AsyncDelegateCommandGeneric.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Serial.MVVM
+{
+    public class AsyncDelegateCommand<T> : AsyncDelegateCommand
+    {
+        public new Func<T, Task> Execute { get; set; }
+
+        private async Task CallAsyncExecuteGeneric(object parameter)
+        {
+            await Execute((T)parameter);
+        }
+
+        public AsyncDelegateCommand(Func<T, Task> execute) : this(execute, true)
+        {
+        }
+
+        public AsyncDelegateCommand(Func<T, Task> execute, bool canExecute) : base((Func<object, Task>)null, canExecute)
+        {
+            Execute = execute;
+            base.Execute = CallAsyncExecuteGeneric;
+        }
+    }
+}
